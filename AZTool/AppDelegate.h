214e6088f781e79a767243580cae71f1b4694910
AZName <964190389@qq.com>
2017-08-01 18:30:17 +0800

    Initial Commit

---
+++ AZTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZTool
+//
+//  Created by 徐振 on 2017/8/1.
+//  Copyright © 2017年 徐振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
